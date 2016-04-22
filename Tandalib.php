2631ca94f97892c58ba10fff4cf853c878c74b9b
Scott Barber <barbz@barbz.com.au>
2016-04-22 10:49:30 +1000

    Create Tandalib.php

---
+++ Tandalib.php
@@ -0,0 +1,101 @@
+<?php
+
+class Tandalib {
+    //https://my.tanda.co/api/v2/documentation
+
+    private $user = ""; //add more stuff here
+    private $pass = ""; //add more stuff here
+    private $token = "";
+    private $defaultscopes = "roster"; //add more stuff here
+    private $reqBase = "https://my.tanda.co/api/v2/";
+
+    public function __construct() {
+
+        //this should actually read the database and get the token out of there.
+        //implementation below creates a new one every time - bad.
+
+        /*
+          $tokenObj = get_db_token();
+          if ($tokenObj['expired']) {
+          $tokenObj = $this->auth_pw($this->$defaultscopes);
+          }
+          $this->token = $token['access_token'];
+         * 
+         */
+
+        $this->token = $this->auth_pw($this->$defaultscopes);
+    }
+
+    public function auth_pw($scopes) { //return token
+        $headers = array("Cache-Control: no-cache");
+        $data = array(
+            "username" => $this->user,
+            "password" => $this->pass,
+            "scope" => $scopes,
+            "grant_type" => "password"
+        );
+        $auth = $this->curl_post("https://my.tanda.co/api/oauth/token", $headers, $data);
+        return $auth;
+    }
+
+    public function curl_auth_get($url) {
+        $headers = array(
+            "Cache-Control: no-cache",
+            sprintf("Authorization: bearer %s", $this->token)
+        );
+
+        $fullUrl = $this->reqBase . $url;
+        return $this->curl_get($this->reqBase . $url, $headers);
+    }
+
+    private function curl_get($url, $headers) {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => $headers
+        ));
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        return $resp;
+    }
+
+    public function curl_auth_post($url, $data){
+        //#todo - written but not tested.
+        $headers = array(
+            "Content-Type: application/json",
+            sprintf("Authorization: bearer %s", $this->token)
+        );
+        
+        return $this->curl_post($url, $headers, $data);
+    }
+    
+    private function curl_post($url, $headers, $data) {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+        curl_setopt($curl, CURLOPT_POST, sizeof($data));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($server_output, true);
+    }
+
+}
+
+
+
+$tib = new Tandalib();
+$rosters = $tib->curl_auth_get("rosters/on/2016-04-18");
+var_dump($rosters);
+
+?>
